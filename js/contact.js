184da28018ffa9fead56e5d14073b6ac84049197
bkrzakii <zackstorm0404@gmail.com>
2025-04-02 21:14:53 +0100

    contact v1.1

---
+++ js/contact.js
@@ -0,0 +1,17 @@
+window.addEventListener("scroll", function() {
+    let header = document.querySelector("header");
+
+    if (window.scrollY > 100) { 
+        header.style.position = "fixed"; /* Stick to the top */
+        header.style.top = "0"; 
+        header.style.background = "#392761ff"; /* Change background */
+        header.style.boxShadow = "0px 2px 10px rgba(0, 0, 0, 0.1)";
+        header.style.padding = "10px 70px"; /* Reduce padding */
+    } else {
+        header.style.position = "absolute"; /* Stay inside .background */
+        header.style.top = "0px"; 
+        header.style.background = "#392761ff"; /* Keep background transparent */
+        header.style.boxShadow = "none";
+        header.style.padding = "10px 30px"; /* Reset padding */
+    }
+});
