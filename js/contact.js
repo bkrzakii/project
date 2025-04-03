0b2b1ac26bea83091c0306a03627443e949b8bb7
bkrzakii <zackstorm0404@gmail.com>
2025-04-03 18:41:27 +0100

    modification

--- js/contact.js
+++ js/contact.js
@@ -6,12 +6,14 @@ window.addEventListener("scroll", function() {
         header.style.top = "0"; 
         header.style.background = "#392761ff"; /* Change background */
         header.style.boxShadow = "0px 2px 10px rgba(0, 0, 0, 0.1)";
-        header.style.padding = "10px 70px"; /* Reduce padding */
+        header.style.width = "100%";
+        header.style.animation = "movedown 1.9s ease-in-out forwards"; /* Smooth transition */
     } else {
-        header.style.position = "absolute"; /* Stay inside .background */
+        header.style.position = "relative"; /* Stay inside .background */
         header.style.top = "0px"; 
         header.style.background = "#392761ff"; /* Keep background transparent */
         header.style.boxShadow = "none";
         header.style.padding = "10px 30px"; /* Reset padding */
+        header.style.width = "none"; /* Ensure full width */
     }
 });
