d80bad2a493df67475c0ae4a92dfc79df5efe1c3
bkrzakii <zackstorm0404@gmail.com>
2025-04-04 00:39:18 +0100

    v2.1

--- js/home.js
+++ js/home.js
@@ -4,14 +4,12 @@ window.addEventListener("scroll", function() {
     if (window.scrollY > 100) { 
         header.style.position = "fixed"; /* Stick to the top */
         header.style.top = "0"; 
-        header.style.background = "#Fbfbfa"; /* Change background */
         header.style.boxShadow = "0px 2px 10px rgba(0, 0, 0, 0.1)";
         header.style.width = "100%"; /* Reduce padding */
         header.style.animation = "movedown 1.9s ease-in-out forwards"; /* Smooth transition */
     } else {
         header.style.position = "relative"; /* Stay inside .background */
         header.style.top = "0px"; 
-        header.style.background = "transparent"; /* Keep background transparent */
         header.style.boxShadow = "none";
         header.style.padding = "10px 30px"; /* Reset padding */
         header.style.width = "none"; /* Ensure full width */
