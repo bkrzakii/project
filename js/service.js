6c7bf2cf05e8f7aa85c64a00af8428c485d2dc9b
firassejr18 <sadifirasse33@gmail.com>
2025-04-07 13:14:35 +0100

    v3.108

--- js/service.js
+++ js/service.js
@@ -4,24 +4,21 @@ window.addEventListener("scroll", function() {
     if (window.scrollY > 100) { 
         header.style.position = "fixed"; /* Stick to the top */
         header.style.top = "0"; 
-        header.style.background = "#392761ff"; /* Change background */
         header.style.boxShadow = "0px 2px 10px rgba(0, 0, 0, 0.1)";
-        header.style.width = "100%"; /* Ensure full width */
+        header.style.width = "none";
         header.style.animation = "movedown 1.9s ease-in-out forwards"; /* Smooth transition */
-        nav.style.margin = "10px 300px"; /* Reduce padding */
     } else {
         header.style.position = "relative"; /* Stay inside .background */
         header.style.top = "0px"; 
-        header.style.background = "#392761ff"; /* Keep background transparent */
         header.style.boxShadow = "none";
-        header.style.padding = "10px 30px"; /* Reset padding */
         header.style.width = "none"; /* Ensure full width */
+        header.style.animation = "movedown 1.9s ease-in-out forwards"; /* Smooth transition */
     }
 });
 function toggleDescription() {
     var desc = document.getElementById("user-info");
-    if (desc.style.display === "none") {
+    if (desc.style.display === "") {
         desc.style.display = "flex";
     } else {
-        desc.style.display = "none";
+        desc.style.display = "";
 }}
